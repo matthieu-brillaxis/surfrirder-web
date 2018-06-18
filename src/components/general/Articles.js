8da3ca5775546f2e5127c59e8de831dd9493d676
ecv <ecv@ECV-BO-B018-IM041.local>
2018-06-18 15:59:48 +0200

    Page Article

--- src/components/general/Articles.js
+++ src/components/general/Articles.js
@@ -7,31 +7,43 @@ import Button from './../general/Button';
 class Articles extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'all', articles: this.props.data};
+    this.state = {value: 'all', articles: this.props.data, limit: 5};
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
+    this.setState({limit: 5});
     this.setState({value: event.target.value});
     if (event.target.value === 'all') {
       this.state.articles = this.props.data;
     } else {
-      this.state.articles = this.props.data.filter(article => article.acf.categorie === this.state.value);
+      this.state.articles = this.props.data.filter(article => article.acf.categorie === event.target.value);
     }
   }
 
   handleClick = (event) => {
-    console.log('lol');
+    this.setState({limit: this.state.limit + 5});
   };
 
   render() {
     console.log(this.props.data);
-    const Article = styled.div`
+    const Articles = styled.div`
       display: flex;
       flex-direction: column;
       background-color: #3BA9BC;
       width: 100%;
       padding: 100px 20px;
+    `;
+
+    const Select = styled.select`
+      color: white;
+      border: 1px solid white;
+      background-color: transparent;
+      padding: 10px;
+    `;
+
+    const Article = styled.div`
+    border-bottom: 1px solid white;
     `;
 
     const H5 = styled.h5`
@@ -43,8 +55,8 @@ class Articles extends Component {
     `;
   
     return (
-      <Article>
-      <select id="filter" value={this.state.value} onChange={this.handleChange}>
+      <Articles>
+      <Select id="filter" value={this.state.value} onChange={this.handleChange}>
         <option value="all">Tous les articles</option> 
         <option value="Amenagement du littoral">Amenagement du littoral</option> 
         <option value="Dechets aquatiques">Dechets aquatiques</option>
@@ -52,18 +64,19 @@ class Articles extends Component {
         <option value="Ocean et Climat">Ocean et Climat</option>
         <option value="Qualité des eaux">Qualité des eaux</option>
         <option value="Surf et Patrimoine">Surf et Patrimoine</option>
-      </select>
-      { this.state.articles.map((prop, key) => {
+      </Select>
+      { this.state.articles.slice(0, this.state.limit).map((prop, key) => {
         return (
-          <div key={key}>
+          <Article key={key}>
             <H5>{prop.title.rendered}</H5>
             <DateArticle>{prop.date}</DateArticle>
-          </div>
+          </Article>
         );
      })}
-     <Button buttonTitle={'Charger plus d\'articles'} onClick={() => this.handleClick}/>
-     <button onClick={this.handleClick}>Activate Lasers</button>
-      </Article>
+     <div onClick={this.handleClick}>
+     <Button buttonTitle={'Charger plus d\'articles'}/>
+     </div>
+      </Articles>
     );
   }
 }
